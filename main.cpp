360f347f4c4c7fb50332c6f3ac3d6125ddd93fca
snir2g1 <snir2g1@localhost>
2018-01-18 17:24:50 +0100

    test git

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   main.cpp
+ * Author: snir2g1
+ *
+ * Created on 18 janvier 2018, 17:15
+ */
+
+#include <QApplication>
+using namespace std;
+int main(int argc, char *argv[]) {
+  
+    cout << "test ">>endl;
+    //test git
+    QApplication app(argc, argv);
+
+
+
+    return app.exec();
+}
